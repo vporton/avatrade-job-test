b0e09fe9017ee684478ede36679eff3d62c7237f
Victor Porton <porton@narod.ru>
2020-10-01 15:35:59 +0300

    testing

--- mytest/tests.py
+++ mytest/tests.py
@@ -7,6 +7,16 @@ class FullTestCase(TestCase):
 
     def test_auth(self):
         self.assertEqual(self.client.post('/user/signup', {'username': 'aa', 'password': 'xx'}).json(),
+                         {'code': 'PAR_01', 'field': 'email', 'message': 'Missing HTTP param.'},
+                         "Missing email not detected.")
+        self.assertEqual(self.client.post('/user/signup', {'username': 'aa', 'email': 'porton@narod.ru'}).json(),
+                         {'code': 'PAR_01', 'field': 'password', 'message': 'Missing HTTP param.'},
+                         "Missing password not detected.")
+        self.assertEqual(self.client.post('/user/signup', {'password': 'xx', 'email': 'porton@narod.ru'}).json(),
+                         {'code': 'PAR_01', 'field': 'username', 'message': 'Missing HTTP param.'},
+                         "Missing username not detected.")
+
+        self.assertEqual(self.client.post('/user/signup', {'username': 'aa', 'password': 'xx', 'email': 'porton@narod.ru'}).json(),
                          {'code': 'USR_04', 'message': 'Password too weak.', 'field': 'password'},
                          "Weak password not detected.")
 
